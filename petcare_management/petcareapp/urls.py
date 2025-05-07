8fc093cbd1c8a53cbbfa25ea97029c5a3dc1f526
Vijitha-DEV <vijithavijayan311@gmail.com>
2025-05-07 13:22:21 +0530

    Update urls.py

--- petcare_management/petcareapp/urls.py
+++ petcare_management/petcareapp/urls.py
@@ -13,7 +13,12 @@ urlpatterns = [
     path('districts/',district_list,    name='district_list'),
     path('select-district/', select_district,  name='select_district'),
     path('hostels/<int:district_id>/', views.hostels_by_district),
+    path('hospitals/<int:district_id>/', views.hospitals_by_district),
+    path('supplies/<int:district_id>/', views.supplies_by_district),
 
+    path('categories/', views.PetCategoryListView.as_view(), name='categories'),
+    path('doctors/', views.DoctorListView.as_view(), name='doctors'),
+    path('appointments/', views.AppointmentCreateView.as_view(), name='appointments'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
